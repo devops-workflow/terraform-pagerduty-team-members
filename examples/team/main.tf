56c42fa1e0e8a8e63c621079860056d95265370b
Steven Nemetz <snemetz@wiser.com>
2018-04-13 13:18:33 -0700

    cleanup

--- examples/team/main.tf
+++ examples/team/main.tf
@@ -13,6 +13,6 @@ module "teams" {
 
 module "team_members" {
   source        = "../../"
-  member_emails = ["snemetz@wiser.com"]
+  member_emails = ["user1@example.com", "user2@example.com", "user3@example.com"]
   team_id       = "${element(module.teams.ids, 0)}"
 }
